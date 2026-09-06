c89a49dd1ba379d342b06864b0adf8d54cf43e3b
Adonis Costa <adoniscosta123@gmail.com>
2026-09-06 11:58:59 -0300

    Projeto Bingo
    Um jogo de bingo simples feito em Python, que sorteia números aleatórios de 1 a 80, sem repetição, até o usuário decidir encerrar

---
+++ bingo.py
@@ -0,0 +1,23 @@
+import random
+bingo = []
+print('Projeto Bingo')
+print('_____________')
+rodar = input('Deseja seguir com o programa? s/n: ')
+if rodar == 'n':
+    print('Você preferiu não seguir com o programa! Programa encerrado.')
+
+while rodar == 's':
+    numero = random.randint(1, 80)
+    while numero in bingo:
+        print('Esse numero já foi sorteado! Sorteando outro.')
+        numero = random.randint(1, 80)
+
+    bingo.append(numero)
+    print(f'Numero sorteado: {numero}')
+    continuar = input('Continuar? s/n: ')
+
+    if continuar == 'n':
+        print('Programa encerrado!')
+        print(f'Esses foram os numeros sorteados:\n{bingo}')
+        break
+
